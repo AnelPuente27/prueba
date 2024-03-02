581d5960fbd7e8e983f2513567fc3e022f176b10
AnelPuente27 <apuente16959@plataforma-utslp.net>
2024-03-01 22:12:25 -0500

    pruebafind

--- actividad2.sh
+++ actividad2.sh
@@ -8,3 +8,5 @@ cd nuevo/
 touch masita
 echo "$fecha Mi primera chamba" > masita
 
+Adding new line to file at Fri Mar  1 10:10:13 PM EST 2024
+01/03/2024-22:10:13	2806-108e-001a-ab93-4e79-3623-3d7f-75cb.ipv6.infinitum.net.mx 	1.97801
